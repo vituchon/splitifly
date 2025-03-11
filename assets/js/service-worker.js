4d799df49dd8edd4cb229956b57870da137863ac
Vituchon <victor@omniasalud.com>
2025-03-11 11:28:11 -0300

    feat: enable offline support with service worker caching all required assets

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -1,52 +1,90 @@
-const CACHE_NAME = "splitifly-cache-v2";
-const ASSETS_TO_CACHE = [
-  "/assets/html/index.html",
-  "/assets/html/about.html",
-  "/assets/css/main.css",
-  "/assets/js/app/app.js",
-  "/assets/js/app/index.js",
-  "/assets/js/model/api/api.js",
-  "/assets/js/model/group.js",
-  "/assets/js/model/movement.js",
-  "/assets/js/model/price.js",
-  "/assets/js/repositories/common.js",
-  "/assets/js/repositories/entity_memory_storage.js",
-  "/assets/js/repositories/movements_memory_storage.js",
-  "/assets/js/repositories/participant-movements-memory-storage.js",
-  "/assets/js/repositories/participants_memory_storage.js",
-  "/assets/js/util/big-fraction.js",
-  "/assets/js/util/fraction.js",
-  "/assets/js/util/number.js",
-  "/assets/images/catty.jpg",
-  "/assets/images/worker.jpeg"
-];
+// code taken and little adapted from: https://bncb2v.csb.app/
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open('v1');
+  await cache.addAll(resources);
+};
 
-self.addEventListener("install", (event) => {
+const putInCache = async (request, response) => {
+  const cache = await caches.open('v1');
+  await cache.put(request, response);
+};
+
+const cacheFirst = async ({ request, preloadResponsePromise }) => {
+  // First try to get the resource from the cache
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+
+  // Next try to use the preloaded response, if it's there
+  const preloadResponse = await preloadResponsePromise;
+  if (preloadResponse) {
+    console.info('using preload response', preloadResponse);
+    putInCache(request, preloadResponse.clone());
+    return preloadResponse;
+  }
+
+  // Next try to get the resource from the network
+  try {
+    const responseFromNetwork = await fetch(request);
+    // response may be used only once
+    // we need to save clone to put one copy in cache
+    // and serve second one
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (error) {
+    // when is not available,
+    // there is nothing we can do, but we must always
+    // return a Response object
+    return new Response('Network error happened', {
+      status: 408,
+      headers: { 'Content-Type': 'text/plain' },
+    });
+  }
+};
+
+const enableNavigationPreload = async () => {
+  if (self.registration.navigationPreload) {
+    // Enable navigation preloads!
+    await self.registration.navigationPreload.enable();
+  }
+};
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(enableNavigationPreload());
+});
+
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+    addResourcesToCache([
+      "/assets/html/index.html",
+      "/assets/html/about.html",
+      "/assets/css/main.css",
+      "/assets/js/app/app.js",
+      "/assets/js/app/index.js",
+      "/assets/js/model/api/api.js",
+      "/assets/js/model/group.js",
+      "/assets/js/model/movement.js",
+      "/assets/js/model/price.js",
+      "/assets/js/repositories/common.js",
+      "/assets/js/repositories/entity_memory_storage.js",
+      "/assets/js/repositories/movements_memory_storage.js",
+      "/assets/js/repositories/participant-movements-memory-storage.js",
+      "/assets/js/repositories/participants_memory_storage.js",
+      "/assets/js/util/big-fraction.js",
+      "/assets/js/util/fraction.js",
+      "/assets/js/util/number.js",
+      "/assets/images/catty.jpg",
+      "/assets/images/worker.jpeg"
+    ])
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
+    cacheFirst({
+      request: event.request,
+      preloadResponsePromise: event.preloadResponse,
     })
   );
 });
